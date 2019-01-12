5e35c3b2112fee852140bc31bd724f690539be8a
Alessio Fiore <alessiofiore@gmail.com>
2019-01-12 22:26:29 +0100

    project files

---
+++ src/main/java/com/springcloud/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.zuul.ZuulFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+	ZuulFilter filter() {
+		return new AddCustomHeaderFilter();
+	}
+
+}
+
